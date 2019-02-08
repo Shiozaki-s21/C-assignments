cf080a132cbd901ea175a639f7cb89b2525dd70b
Kazuya <shiozaki5621@gmail.com>
2019-02-08 10:50:48 -0800

    add assignment4

---
+++ Assignments/Assignment4/Exercise02/vc_strstr.c
@@ -0,0 +1,49 @@
+/*
+ * @Author: Kazuya Takahashi 
+ * @Date: 2019-02-06 15:40:34 
+ * @Last Modified by: Kazuya Takahashi
+ * @Last Modified time: 2019-02-07 14:50:13
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+char *vc_strstr(char *str, char *to_find)
+{
+    // while for str
+    while (*str != '\0')
+    {
+        // if to find same character between src and to_find
+        if (*str == *to_find)
+        {
+            // while for to_find
+            int counter = 0;
+            bool flag = true;
+            while (*to_find != '\0')
+            {
+                //if to find different character between src and to_find
+                if (*str != *to_find)
+                {
+                    // back to while for str
+                    flag = false;
+                }
+                counter++;
+                to_find++;
+                str++;
+            }
+            // flag = true
+            // back to head of to_find with src
+            while (0 < counter)
+            {
+                to_find--;
+                str--;
+                counter--;
+            }
+
+            if (flag)
+                return str;
+        }
+        // count up with src and to_find
+        str++;
+    }
+    return '\0';
+}
---
+++ Assignments/Assignment4/Exercise07/vc_strcapitalize.c
@@ -0,0 +1,56 @@
+/*
+ * @Author: Kazuya Takahashi 
+ * @Date: 2019-02-06 15:40:34 
+ * @Last Modified by: Kazuya Takahashi
+ * @Last Modified time: 2019-02-07 15:02:59
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+char *vc_strcapitalize(char *str)
+{
+
+    printf("==================\n%1$s\n==================\n", __func__);
+    int counter = 0;
+    bool flag = false;
+    while (*str != '\0')
+    {
+        // for head character
+        if (counter == 0)
+        {
+            if (97 <= *str && *str <= 122)
+            {
+                *str = *str - 32;
+                flag = false;
+            }
+        }
+
+        // to upcase
+        if (flag)
+        {
+            if (97 <= *str && *str <= 122)
+            {
+                *str = *str - 32;
+                flag = false;
+            }
+        }
+
+        // if *str is space or + or -, flag
+        if (*str == 32 || *str == 43 || *str == 45)
+        {
+            flag = true;
+        }
+
+        *str++;
+        counter++;
+    }
+
+    //go back to head
+    while (0 < counter)
+    {
+        counter--;
+        *str--;
+    }
+
+    return str;
+}
